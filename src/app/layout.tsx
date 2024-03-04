5abd42bcb7ab8bec17dfe4116c23ae7662d08668
Ashwani Singh <singhashwanikumar793@gmail.com>
2024-03-04 12:30:35 +0530

    wrapped page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import { ApolloProvider } from "@apollo/client";
-import client from "@/lib/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ApolloProvider client={client}>
-          <Navbar />
-          <main>{children}</main>
-        </ApolloProvider>
+        <Navbar />
+        <main>{children}</main>
         {/* <Footer /> */}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Anime from "./anime/page";
 import Hobbies from "./components/Hobbies";
 
 const page = () => {
+  const client = new ApolloClient({
+    uri: "https://graphql.anilist.co",
+    cache: new InMemoryCache(),
+  });
   return (
     <>
-      <Anime />
+      <ApolloProvider client={client}>
+        <Anime />
+      </ApolloProvider>
     </>
   );
 };
